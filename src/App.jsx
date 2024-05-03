19c0b9f8a669bfe46d9a5e85b5090351f470784a
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-05-03 19:35:41 +0300

    add banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import UpperMenu from './components/upper-menu/upper-menu'
-import './App.css'
+import Banner from './components/banner/banner'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <UpperMenu></UpperMenu>
+    <>
+      <UpperMenu></UpperMenu>
+      <div className={styles['main']}>
+        <Banner></Banner>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/banner.module.css'
+
+export default function Banner() {
+    return (
+        <div className={styles['banner']}>
+            <h1>Coffee Explorer</h1>
+        </div>
+    )
+}
