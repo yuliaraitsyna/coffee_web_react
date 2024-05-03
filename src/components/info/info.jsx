98a6858b82ed2aa8b698917139404dedb90265e3
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-05-03 21:34:57 +0300

    add data and implement display of text

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import styles from '../../styles/info.module.css'
 
 export default function Info() {
+    const [text, setText] = useState(null);
+
+    useEffect(() => {
+        fetch('../../../data/text.json')
+            .then(response => response.json())
+            .then(data => setText(data.info_section.text))
+            .catch(error => console.error(error));
+    }, []);
+
     return (
         <div className={styles['container']}>
             <div className={styles['text']}>
                 <h2>What is coffee?</h2>
                 <p>
-
+                    {text}
                 </p>
             </div>
             <div className={styles['img']}></div>
