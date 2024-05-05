98a9f0aa50226000ff63f86666f31c4d49bac10a
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-05-05 17:15:51 +0300

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: '/coffee_web_react/',
   plugins: [
     react(),
   ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/login/index.html'),
+        menu: resolve(__dirname, 'src/menu/index.html'),
+        generator: resolve(__dirname, 'src/generator/index.html'),
+        about: resolve(__dirname, 'src/about/index.html')
+      }
+    }
+  }
 });
