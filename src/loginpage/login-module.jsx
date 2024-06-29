461145a514dffaa8946b995a1978915a6d39a059
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-06-30 00:02:49 +0300

    fix: add environment variables

--- src/loginpage/login-module.jsx
+++ src/loginpage/login-module.jsx
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD16XjF3aln0rNL8MVP7ibCnYmRCeCnrBg",
-    authDomain: "coffee-generator-auth.firebaseapp.com",
-    projectId: "coffee-generator-auth",
-    storageBucket: "coffee-generator-auth.appspot.com",
-    messagingSenderId: "1030366226948",
-    appId: "1:1030366226948:web:4277384c3870a84dc8a351"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  base: '/coffee_web_react/',
-  plugins: [
-    react(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: '/coffee_web_react/',
+    plugins: [
+      react(),
+    ],
+    define: {
+      'process.env': env
+    }
+  };
 });
