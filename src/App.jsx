77cb4f0ae87a5b0da71425bd6096d38e7a67ee7d
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-06-29 23:37:08 +0300

    feat: add router for login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import MenuPage from "./menupage/menupage";
 import UpperMenu from "./components/upper-menu/upper-menu";
 import Footer from "./components/footer/footer";
 import GeneratorPage from "./generatorpage/generatorpage";
-
+import { AuthProvider } from "./loginpage/login-module";
+import LoginPage from "./loginpage/loginpage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/coffee_web_react/menu" element={<MenuPage />} />
           <Route path="/coffee_web_react/about" element={<></>}/>
           <Route path="/coffee_web_react/generator" element={<GeneratorPage />} />
+          <Route path="/coffee_web_react/login" element={<AuthProvider><LoginPage/></AuthProvider>} />
         </Routes>
       <Footer/>
      </Router>
